d109e6f6c2a9f13cbe387fceb8edbfdcfdcca2bc
Scott R. Santos <santos@auburn.edu>
2016-09-30 11:30:40 -0500

    Copied script over from old install

---
+++ weather_report.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+LOCWEATHER="`wunderground.py | grep -E '(Location|Condition|Temperature)' | sed 's/Location : //;s/Condition : /: /;s/Temperature :/ -- /;s/ C)/°C)/;s/ F /°F /' | xargs`"
+
+if ! [ "`ping -c 1 google.com`" ]; then
+echo ": N/A"
+else
+echo ": ${LOCWEATHER}"
+fi
+exit
