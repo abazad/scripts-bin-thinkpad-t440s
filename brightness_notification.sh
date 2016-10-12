205c19f3eee0356a2e17e09e63e7e52951d81ab8
Scott R. Santos <santos@auburn.edu>
2016-10-11 22:30:56 -0500

    Added environmental variables and notify-send attributes to the script

--- brightness_notification.sh
+++ brightness_notification.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-sudo -u srsantos /usr/bin/paplay --server /run/user/1000/pulse/native /home/srsantos/.i3/sounds/Click1.wav > /dev/null 2>&1
+export XAUTHORITY=/home/srsantos/.Xauthority
+export DISPLAY=:0
+export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
+/usr/bin/sudo -u srsantos /usr/bin/paplay --server /run/user/1000/pulse/native /home/srsantos/.i3/sounds/Click1.wav > /dev/null 2>&1
 
-
-#export XAUTHORITY=/home/srsantos/.Xauthority
-#export DISPLAY=:0
-#export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
-
-#/usr/bin/notify-send -i /usr/share/icons/gnome/256x256/status/battery-full-charging.png 'Changing Power States' --expire-time=4000
-#/usr/bin/paplay /home/srsantos/.i3/sounds/Click1.wav > /dev/null 2>&1
+/usr/bin/notify-send -i /usr/share/icons/gnome/256x256/status/battery-full-charging.png 'Changing Power States' --expire-time=4000
