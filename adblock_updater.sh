98cc7c56fbe4672d57cba3b46cfbd966e1364fc9
Scott R. Santos <santos@auburn.edu>
2017-06-24 07:51:18 +0200

    Changed notifications from notify-send to systemd journal

--- adblock_updater.sh
+++ adblock_updater.sh
@@ -5,9 +5,9 @@ export DISPLAY=:0
 export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 
 if ! [ "`ping -4 -c 1 google.com`" ]; then
-    notify-send -u critical "Currently offline." "Will check for updated Fanboy adblock list later..."
+    /usr/bin/systemd-cat -t "Fanboy_Adblock" /usr/bin/echo "***** Currently offline. Will check for updated Fanboy adblock list later... *****"
 else
     wget --quiet https://www.fanboy.co.nz/r/fanboy-ultimate.txt --output-document=/home/srsantos/.local/share/luakit/adblock/fanboy-ultimate.txt
-    notify-send -u normal -i /usr/share/icons/gnome/256x256/status/security-medium.png 'Fanboy adblock list updated.'
+    /usr/bin/systemd-cat -t "Fanboy_Adblock" /usr/bin/echo "***** FANBOY ADBLOCK LIST FOR LUAKIT UPDATED ON $(date) *****"
 fi
 exit 0
