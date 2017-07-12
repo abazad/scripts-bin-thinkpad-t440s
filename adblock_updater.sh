fb6da45a128fa6393de1c7248aff0b85044ca6d4
Scott R. Santos <santos@auburn.edu>
2017-07-12 08:13:24 -0500

    Added removal of old file to potentially avoid wget errors

--- adblock_updater.sh
+++ adblock_updater.sh
@@ -7,6 +7,7 @@ export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
 if ! [ "`ping -4 -c 1 google.com`" ]; then
     /usr/bin/systemd-cat -t "Fanboy_Adblock" /usr/bin/echo "***** Currently offline. Will check for updated Fanboy adblock list later... *****"
 else
+    rm -f /home/srsantos/.local/share/luakit/adblock/fanboy-ultimate.txt
     wget --quiet https://www.fanboy.co.nz/r/fanboy-ultimate.txt --output-document=/home/srsantos/.local/share/luakit/adblock/fanboy-ultimate.txt
     /usr/bin/systemd-cat -t "Fanboy_Adblock" /usr/bin/echo "***** FANBOY ADBLOCK LIST FOR LUAKIT UPDATED ON $(date) *****"
 fi
