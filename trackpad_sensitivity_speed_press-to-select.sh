23a27d7c472f6555577e05b8b8c0e2ad051107b4
Scott R. Santos <santos@auburn.edu>
2017-08-25 23:15:42 -0500

    Various tweaks based on config changes

--- trackpad_sensitivity_speed_press-to-select.sh
+++ trackpad_sensitivity_speed_press-to-select.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-/usr/bin/echo -n "1" > /sys/devices/platform/i8042/serio1/serio2/press_to_select
-/usr/bin/echo -n "235" > /sys/devices/platform/i8042/serio1/serio2/sensitivity
-/usr/bin/echo -n "90" > /sys/devices/platform/i8042/serio1/serio2/speed
-/usr/bin/echo -n "200" > /sys/devices/platform/i8042/serio1/serio2/rate
+#/usr/bin/echo -n "1" > /sys/devices/platform/i8042/serio1/serio2/press_to_select
+/usr/bin/echo -n "220" > /sys/devices/platform/i8042/serio1/serio2/sensitivity
+#/usr/bin/echo -n "90" > /sys/devices/platform/i8042/serio1/serio2/speed
+#/usr/bin/echo -n "200" > /sys/devices/platform/i8042/serio1/serio2/rate
 /usr/bin/echo -n "25" > /sys/devices/platform/i8042/serio1/serio2/drift_time
-/usr/bin/echo -n "6" > /sys/devices/platform/i8042/serio1/serio2/inertia
+#/usr/bin/echo -n "6" > /sys/devices/platform/i8042/serio1/serio2/inertia
 
 /usr/bin/systemd-cat -t "Trackpoint" /usr/bin/echo "***** MODIFYING TRACKPOINT ATTRIBUTES ON $(date) *****"
